a1e492325fb3de5b5e1ee22fe8044201c81089a9
Aces45 <stevenignacio45@yahoo.com>
2024-05-01 16:09:26 +0800

    added placeholder text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,18 @@
 <template>
   <h1>About Us</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem exercitationem similique sed facere quisquam alias? Autem, sed? Porro voluptatem, earum est cupiditate, numquam sed eos et vel consectetur eligendi perferendis asperiores iure assumenda. Dolores iure error temporibus numquam facilis enim architecto tempore mollitia debitis consectetur ea, accusamus dicta placeat laborum unde quisquam itaque nihil? Repellendus consectetur earum inventore aliquam rerum quia ipsam consequatur repellat nulla dignissimos et quaerat modi qui facere voluptate quibusdam sequi ipsa enim, commodi error adipisci quasi aut molestiae? Tenetur quis architecto eveniet placeat harum quos porro iure magnam delectus, corrupti error est amet ducimus necessitatibus.</p>
+  <p class="paragraph">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem
+    exercitationem similique sed facere quisquam alias? Autem, sed? Porro
+    voluptatem, earum est cupiditate, numquam sed eos et vel consectetur
+    eligendi perferendis asperiores iure assumenda. Dolores iure error
+    temporibus numquam facilis enim architecto tempore mollitia debitis
+    consectetur ea, accusamus dicta placeat laborum unde quisquam itaque nihil?
+    Repellendus consectetur earum inventore aliquam rerum quia ipsam consequatur
+    repellat nulla dignissimos et quaerat modi qui facere voluptate quibusdam
+    sequi ipsa enim, commodi error adipisci quasi aut molestiae? Tenetur quis
+    architecto eveniet placeat harum quos porro iure magnam delectus, corrupti
+    error est amet ducimus necessitatibus.
+  </p>
 </template>
 
 <script>
@@ -9,11 +21,12 @@ export default {
 }
 </script>
 
-<style scoped>
-p{
+<style>
+
+.paragraph {
   font-size: 1.5rem;
   margin: 0 5rem;
-  padding: 0 250px;
+  padding: 0 2rem;
   text-align: justify;
   text-indent: 50px;
 }
