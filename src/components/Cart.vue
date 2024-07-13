6be56f41e5c0c2f60420ea2a2b0675a0f1179e8c
Aces45 <stevenignacio45@yahoo.com>
2024-07-13 14:51:08 +0800

    added cart component

---
+++ src/components/Cart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="cart">
+        <h1>This is the cart!</h1>
+        <h2>test</h2>
+    </div>
+</template>
+
+<script>
+
+export default ({
+    setup() {
+
+    }
+})
+</script>
