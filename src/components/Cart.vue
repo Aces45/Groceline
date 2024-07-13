9961b47be68442d78ecb65a2fbcfde298905842a
Aces45 <stevenignacio45@yahoo.com>
2024-07-13 20:21:15 +0800

    created cart content and added logic for closing cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,20 +1,40 @@
 <template>
   <div class="cart">
-    <button>TEST</button>
+    <button @click="closeCart()" style="width: 25%; margin: 0 auto"><i class="fa-solid fa-xmark"></i></button>
     <h1>This is the cart!</h1>
-    <h2>test</h2>
+    <table>
+        <tr>
+            <td>ID</td>
+            <td>Item</td>
+            <td>Quantity</td>
+        </tr>
+        <tbody>
+            <tr v-for="item in this.cart" :key="item.id">
+                <td>{{ item.id }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.quantity }}</td>
+            </tr>
+        </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 
 export default ({
+  props: ['cart'],
   setup() {
   },
   methods: {
-    holdCart(){
-        this.$emit('holdCart')
+    closeCart(){
+        this.$emit('closeCart')
     }
   }
 })
 </script>
+
+<style>
+table, td, tr {
+    border: 1px solid black;
+}
+</style>
