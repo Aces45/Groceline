b502cc2de3d479b90d5a4b39ca78a4492f6b2fc1
Aces45 <stevenignacio45@yahoo.com>
2024-07-13 20:53:30 +0800

    added cart titles

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="cart">
-    <button @click="closeCart()" style="width: 25%; margin: 0 auto"><i class="fa-solid fa-xmark"></i></button>
-    <h1>This is the cart!</h1>
+    <button class="close-btn" @click="closeCart()"><i class="fa-solid fa-xmark"></i></button>
+    <h2 v-if="cart.length == 0">Your cart is empty.</h2>
+    <h2 v-else-if="cart.length == 1">You have 1 item in your cart.</h2>
+    <h2 v-else>You have {{ cart.length }} items in your cart.</h2>
     <table>
         <tr>
             <td>ID</td>
