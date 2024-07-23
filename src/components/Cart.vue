743f1cc0cfef3be73b14389ebf5ef6d3b5e4715a
Aces45 <stevenignacio45@yahoo.com>
2024-07-23 21:16:16 +0800

    fixed item quantity buttons

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -22,8 +22,12 @@
               <tr v-for="item in this.cart" :key="item.id">
                   <td><img :src="item.img" height="100px" width="100px"/></td>
                   <td>{{ item.name }}</td>
-                  <td><input type="number" v-model="item.quantity" title="Quantity" min="0"
-                    style="font-size: 1.1rem; max-width: 20%; text-align: center; border-radius: 5px; border: 2px solid #93C47D"></td>
+                  <td>
+                    <button class="cart-btn" @click="item.quantity--">-</button>
+                    <input type="number" v-model="item.quantity" title="Quantity" min="0"
+                    style="font-size: 1.1rem; max-width: 20%; text-align: center; border-radius: 5px; border: 2px solid #93C47D">
+                    <button class="cart-btn" @click="item.quantity++">+</button>
+                  </td>
                   <td>₱{{ item.price }}</td>
                   <td>₱{{ item.quantity * item.price }}</td>
                   <td><button class="close-btn" @click="removeItem(item.id)" style="margin: 0; height: 40px; width: 40px; cursor: pointer;"><i class="fa-solid fa-trash"></i></button></td>
