30ab3175eb19c221e1850bbad6151e8928f8fd19
Aces45 <stevenignacio45@yahoo.com>
2024-07-23 21:15:37 +0800

    added categories tab

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -11,12 +11,20 @@ import creamPuff5 from '../assets/images/creampuff5.png';
 <template>
   <h1>Shop</h1>
   <hr />
-  <div class="content">
-    <div v-for="item in items" class="itemCard" :key="item.id">
-      <h1>{{ item.name }}</h1>
-      <h3>{{ item.category }}</h3>
-      <img :src="item.img" width="180px" height="180px" />
-      <button @click="addToCart(item)">Add to Cart</button>
+  <div style="display: flex; flex-direction: row; padding: 1% 10%">
+    <div style="background-color: #93C47D; width: 40%; border-radius: 10px; font-size: 1.5rem;">
+      <p>Categories</p>
+      <p class="category" v-for="category in this.categories" :key="category.id" style="cursor: pointer" @click="filterItems(category.name)">
+        {{ category }}
+      </p>
+    </div>
+    <div class="content">
+      <div v-for="item in items" class="itemCard" :key="item.id">
+        <h1>{{ item.name }}</h1>
+        <h3>{{ item.category }}</h3>
+        <img :src="item.img" width="180px" height="180px" />
+        <button @click="addToCart(item)">Add to Cart</button>
+      </div>
     </div>
   </div>
 </template>
