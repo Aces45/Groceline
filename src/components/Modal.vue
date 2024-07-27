28e04bd0fbb46327de9e856f373ab1dd86341250
Aces45 <stevenignacio45@yahoo.com>
2024-07-27 22:58:40 +0800

    added modal properties

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -12,3 +12,16 @@ export default ({
   props: ['message', 'image']
 })
 </script>
+
+<style>
+.modal{
+    z-index: 999;
+    margin-left: 40%;
+    height: auto;
+    width: 500px;
+    position: fixed;
+    border-radius: 10px;
+    background-color: pink;
+}
+
+</style>
