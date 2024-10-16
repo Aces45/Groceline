b24900a2015218ca50c65e59d3646ad05b98c9d2
Aces45 <stevenignacio45@yahoo.com>
2024-10-16 18:04:45 +0800

    added reset label to category reset button

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -20,7 +20,7 @@ import creamPuff5 from '@/assets/images/creampuff5.png';
   <div style="display: flex; flex-direction: row; padding: 1% 10%">
     <div style="background-color: #93C47D; width: 40%; border-radius: 10px; font-size: 1.5rem;">
       <h4 style="color:white">Categories</h4>
-      <button @click = "reset"></button>
+      <button @click = "reset">Reset</button>
       <p class="category" v-for="category in categories" :key="category.id" style="cursor: pointer"
         @click="filterItems(category)">
         {{ category }}
