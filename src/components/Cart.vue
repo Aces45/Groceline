d5c76692669a436582a556219a794cd11ad96d4d
Aces45 <stevenignacio45@yahoo.com>
2024-11-20 10:40:04 +0800

    changed displayed cart quantity to "cartQuantity" prop

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -5,9 +5,9 @@
     display: flex; justify-content: space-around;">
       <p></p>
       <p></p>
-      <h2 v-if="this.cart.length == 0">Your cart is empty.</h2>
-      <h2 v-else-if="this.cart.length == 1">You have 1 item in your cart.</h2>
-      <h2 v-else>You have {{ this.cart.length }} items in your cart.</h2>
+      <h2 v-if="cartQuantity == 0">Your cart is empty.</h2>
+      <h2 v-else-if="cartQuantity == 1">You have 1 item in your cart.</h2>
+      <h2 v-else>You have {{ cartQuantity }} items in your cart.</h2>
       <button class="close-btn" @click="closeCart()" style="justify-self: end; margin: 35px"><i class="fa-solid fa-xmark"></i></button>
     </div>
       <table v-if="this.cart.length > 0">
